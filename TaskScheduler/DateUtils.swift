f86d78ad8047d908374d59d326428786648a9464
Ben Oztalay <boztalay@gmail.com>
2016-01-11 21:56:18 -0500

    Cleaning up DateUtils

--- TaskScheduler/DateUtils.swift
+++ TaskScheduler/DateUtils.swift
@@ -9,23 +9,27 @@
 import Foundation
 
 class DateUtils {
+    // Returns an NSDate for today, without any time components
     static func todayDay() -> NSDate {
-        let calendar = NSCalendar.currentCalendar()
-        return calendar.dateFromComponents(calendar.components([.Year, .Month, .Day], fromDate: NSDate()))!
+        return DateUtils.removeTimeFromDate(NSDate())
     }
     
+    // Returns an NSDate for tomorrow, without any time components
     static func tomorrowDay() -> NSDate {
         return DateUtils.dateByAddingDay(DateUtils.todayDay())
     }
     
+    // Returns an NSDate 24 hours in the future from the given date
     static func dateByAddingDay(date: NSDate) -> NSDate {
         return date.dateByAddingTimeInterval(24 * 60 * 60)
     }
     
+    // Returns an NSDate 24 hours in the past from the given date
     static func dateBySubtractingDay(date: NSDate) -> NSDate {
         return date.dateByAddingTimeInterval(-24 * 60 * 60)
     }
     
+    // Strips the given date of its time components
     static func removeTimeFromDate(date: NSDate) -> NSDate {
         return NSCalendar.currentCalendar().dateBySettingHour(0, minute: 0, second: 0, ofDate: date, options: NSCalendarOptions(rawValue: 0))!
     }
